edfa005ffc4b4d726596ffcc717aae3de454bf6d
Lulloooo <lucagabri98@live.it>
2026-03-02 23:14:20 +0100

    uploading updated model to HF

---
+++ training/trainedModel-HFupload.py
@@ -0,0 +1,12 @@
+from huggingface_hub import HfApi, login, upload_folder
+import os
+
+repo_id = "Lullooo/BTC-volatility-forecasting-model"
+
+login(token=os.environ["HF_TOKEN"])
+
+upload_folder(
+    folder_path="model_output",
+    repo_id=repo_id,
+    repo_type="model",
+)
